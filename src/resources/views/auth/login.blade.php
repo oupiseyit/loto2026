396f2138401cbbac71c9eac739ada2fa5c7561e1
oupiseyit <oupisey.it@gmail.com>
2026-05-10 13:50:10 +0700

    show button at login

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -50,7 +50,7 @@
         </form>
 
         {{-- Quick-fill dev buttons (hidden in production) --}}
-        @unless(app()->isProduction())
+        <!-- @unless(app()->isProduction()) -->
         <div class="flex gap-2 mt-3" x-data>
             @foreach ([['admin','admin','admin123'],['master','master1','master123'],['staff','staff1','staff123']] as [$role,$user,$pass])
             <button type="button"
@@ -61,6 +61,6 @@
             </button>
             @endforeach
         </div>
-        @endunless
+        <!-- @endunless -->
     </div>
 </x-guest-layout>
